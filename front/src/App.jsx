1a70d0d85839373244aef1bcbe9332da0234ba5b
KangSoYeon <aaa0311@naver.com>
2022-05-09 20:50:33 +0900

    [FE] Skeleton code

---
+++ front/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  display: flex;
+`;
+
+function App() {
+  return <Wrapper>책방</Wrapper>;
+}
+
+export default App;
