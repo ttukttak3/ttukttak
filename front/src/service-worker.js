4af0de219bf848978452280bb6bbe00922e88b06
KangSoYeon <aaa0311@naver.com>
2022-05-10 13:56:13 +0900

    [FE] Eslint 설정 값 변경
    PWA를 위한 기본 service-worker와 serviceWorkerRegistration의 코드가
    React airbnb 모드의 eslint 설정값과 맞지 않아서 조건 몇가지 수정

--- front/src/service-worker.js
+++ front/src/service-worker.js
@@ -43,7 +43,7 @@ registerRoute(
 
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html'),
 );
 
 // An example runtime caching route for requests that aren't handled by the
@@ -58,12 +58,12 @@ registerRoute(
       // least-recently used images are removed.
       new ExpirationPlugin({ maxEntries: 50 }),
     ],
-  })
+  }),
 );
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-self.addEventListener('message', (event) => {
+self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
