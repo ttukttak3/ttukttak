f2819927a7f7fbce3be185e3b0007c6138a1f99d
An Hyuck <blackhyuck@gmail.com>
2022-05-23 20:57:06 +0900

    [BE] Skeleton code

---
+++ server/src/test/java/com/ttukttak/TtukttakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttukttak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtukttakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
