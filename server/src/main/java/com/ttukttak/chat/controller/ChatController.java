8ceeda0bbac770a7b549d6dbfb7d260cb425c428
Songwonseok <dnjstjr0517@gmail.com>
2022-06-12 02:15:33 +0900

    Feat: 레디스 pub/sub Service 구현

---
+++ server/src/main/java/com/ttukttak/chat/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.ttukttak.chat.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+import com.ttukttak.chat.dto.ChatMessageDto;
+import com.ttukttak.chat.service.ChatRoomService;
+import com.ttukttak.chat.service.RedisPublisher;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+
+	private final RedisPublisher redisPublisher;
+	private final ChatRoomService chatRoomService;
+
+	/**
+	 * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
+	 */
+	@MessageMapping("/chat/message")
+	public void message(ChatMessageDto message) {
+		// Websocket에 발행된 메시지를 redis로 발행한다(publish)
+		System.out.println("redis 전송 : " + message);
+		redisPublisher.publish(chatRoomService.getTopic(message.getRoomId()), message);
+	}
+}
