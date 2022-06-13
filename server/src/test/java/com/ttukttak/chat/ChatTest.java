e43e6a4315ae9c18f78ef9de15daa84f5745e1f3
Songwonseok <dnjstjr0517@gmail.com>
2022-06-14 00:46:40 +0900

    Test: ChatRoom @OneToMany insert, delete cascade 테스트

--- server/src/test/java/com/ttukttak/chat/ChatTest.java
+++ server/src/test/java/com/ttukttak/chat/ChatTest.java
@@ -1,12 +1,15 @@
 package com.ttukttak.chat;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.ttukttak.chat.entity.ChatMessage;
+import com.ttukttak.book.entity.Book;
+import com.ttukttak.book.repository.BookRepository;
 import com.ttukttak.chat.entity.ChatRoom;
 import com.ttukttak.chat.entity.LastCheckedMessage;
 import com.ttukttak.chat.repository.ChatMessageRepository;
@@ -25,47 +28,71 @@ public class ChatTest {
 	ChatMessageRepository chatMessageRepository;
 
 	@Autowired
+	BookRepository bookRepository;
+
+	@Autowired
 	ChatRoomRepository chatRoomRepository;
 
 	@Autowired
 	LastCheckedMessageRepository lastCheckedMessageRepository;
 
 	@Test
-	@DisplayName("room cascade test")
-	void cascadeTest() {
+	@DisplayName("채팅방 Cascade insert 테스트")
+	void cascadeInsertTest() {
 		User host = User.builder().email("host").build();
 		User guest = User.builder().email("guest").build();
 
 		userRepository.save(host);
 		userRepository.save(guest);
 
-		ChatRoom chatRoom = ChatRoom.builder().host(host).guest(guest).build();
+		Book book = Book.builder().owner(host).build();
+		bookRepository.save(book);
+
+		ChatRoom chatRoom = ChatRoom.builder().book(book).build();
+
+		LastCheckedMessage lastCheckedMessage1 = LastCheckedMessage.builder().user(host).build();
+		LastCheckedMessage lastCheckedMessage2 = LastCheckedMessage.builder().user(guest).build();
+
+		chatRoom.addLastchecedMessage(lastCheckedMessage1);
+		chatRoom.addLastchecedMessage(lastCheckedMessage2);
 
 		chatRoomRepository.save(chatRoom);
 
-		ChatMessage chatMessage = ChatMessage.builder().user(host).message("테스트입니다.").build();
-		chatMessageRepository.save(chatMessage);
+		List<LastCheckedMessage> lastCheckedMessages = chatRoom.getLastCheckedMessages();
 
-		LastCheckedMessage lastCheckedMessage = LastCheckedMessage.builder()
-			.message(chatMessage)
-			.room(chatRoom)
-			.user(host)
-			.build();
-		lastCheckedMessageRepository.save(lastCheckedMessage);
-
-		Long lastMessageId = lastCheckedMessage.getId();
-		Long chatMessageId = chatMessage.getId();
-
+		// 채팅룸 삭제
 		chatRoomRepository.deleteById(chatRoom.getId());
 
-		LastCheckedMessage findLastCheckedMessage = lastCheckedMessageRepository.findById(lastMessageId)
-			.orElse(null);
+		// lastCheckedMessage도 insert 됐는지 확인
+		Assertions.assertThat(lastCheckedMessage1.getId()).isNotNull();
+		Assertions.assertThat(lastCheckedMessage2.getId()).isNotNull();
 
-		ChatMessage findChatMessage = chatMessageRepository.findById(chatMessageId)
-			.orElse(null);
+	}
 
-		Assertions.assertThat(chatMessage.getMessage()).isEqualTo("테스트입니다.");
-		Assertions.assertThat(findLastCheckedMessage).isNull();
-		// Assertions.assertThat(findChatMessage).isNull();
+	@Test
+	@DisplayName("채팅방 Cascade delete 테스트")
+	void cascadeDeleteTest() {
+		User user = User.builder().email("user").build();
+
+		userRepository.save(user);
+
+		Book book = Book.builder().owner(user).build();
+		bookRepository.save(book);
+
+		ChatRoom chatRoom = ChatRoom.builder().book(book).build();
+		chatRoomRepository.save(chatRoom);
+
+		LastCheckedMessage lastCheckedMessage = LastCheckedMessage.builder().room(chatRoom).user(user).build();
+		lastCheckedMessageRepository.save(lastCheckedMessage);
+
+		Long messageId = lastCheckedMessage.getId();
+
+		// 채팅룸 삭제
+		chatRoomRepository.deleteById(chatRoom.getId());
+
+		LastCheckedMessage findMessage = lastCheckedMessageRepository.findById(messageId).orElseThrow();
+
+		// LastCheckedMessage도 delete 됐는지 확인
+		Assertions.assertThat(findMessage).isNull();
 	}
 }
