baf28cb39e72941b6f88dc08ef56bf523f87409b
Songwonseok <dnjstjr0517@gmail.com>
2022-06-14 03:49:03 +0900

    Feat: DTO ChatRoomCard, ChatRoomInfo, ChatRoomRequest,LastMessage 구현
    - ChatRoomCard: 채팅방 리스트 화면 Response
    - ChatRoomInfo: 채팅방 id 정보(무슨 값을 추가해야 할지 고민중)
    - ChatUser: 채팅기능 유저 DTO
    - LastMessage: 채팅방에 올라온 마지막 메시지정보

---
+++ server/src/main/java/com/ttukttak/chat/dto/LastMessage.java
@@ -0,0 +1,9 @@
+package com.ttukttak.chat.dto;
+
+import java.time.LocalDateTime;
+
+public class LastMessage {
+	private String message;
+	private MessageType messageType;
+	private LocalDateTime sendedAt;
+}
