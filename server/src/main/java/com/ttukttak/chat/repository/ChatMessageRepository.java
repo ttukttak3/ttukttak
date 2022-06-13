3661a8b0162c22157d6ea87d9bac79d4523803ab
Songwonseok <dnjstjr0517@gmail.com>
2022-06-14 03:50:57 +0900

    Feat: 채팅메시지 Controller, Service 구현

--- server/src/main/java/com/ttukttak/chat/repository/ChatMessageRepository.java
+++ server/src/main/java/com/ttukttak/chat/repository/ChatMessageRepository.java
@@ -1,8 +1,11 @@
 package com.ttukttak.chat.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ttukttak.chat.entity.ChatMessage;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+	List<ChatMessage> findAllByRoomId(Long roomId);
 }
