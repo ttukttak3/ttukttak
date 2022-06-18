fc967868ef32ac6d187805b4b18b44d960dada97
ANHYUCK8150 <blackhyuck@gmail.com>
2022-06-18 23:27:09 +0900

    feat: [BE] 회원가입 로직 추가 및 소스 수정

--- server/src/main/java/com/ttukttak/oauth/controller/UserController.java
+++ server/src/main/java/com/ttukttak/oauth/controller/UserController.java
@@ -1,26 +1,68 @@
 package com.ttukttak.oauth.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.ttukttak.common.exception.ResourceNotFoundException;
+import com.ttukttak.oauth.dto.SignUpRequest;
+import com.ttukttak.oauth.dto.UserDto;
 import com.ttukttak.oauth.entity.CurrentUser;
-import com.ttukttak.oauth.entity.User;
 import com.ttukttak.oauth.entity.UserPrincipal;
-import com.ttukttak.oauth.repository.UserRepository;
+import com.ttukttak.oauth.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
-    private final UserRepository userRepository;
+	private final UserService userService;
 
-    @GetMapping("/user/me")
-    @PreAuthorize("hasRole('USER')")
-    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
-        return userRepository.findById(userPrincipal.getId())
-                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
-    }
+	/*
+	 * 로그인한 유저정보 가져오기
+	 */
+	@GetMapping("/me")
+	public ResponseEntity<UserDto> getCurrentUser(@CurrentUser
+	UserPrincipal userPrincipal) {
+
+		UserDto userDto = new UserDto(userPrincipal.getUser());
+
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(userDto);
+	}
+
+	/*
+	 * 닉네임 중복 체크
+	 */
+	@GetMapping("/chknickname")
+	public ResponseEntity<Boolean> chkName(@RequestParam
+	String nickname) {
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(userService.existsByName(nickname));
+	}
+
+	/*
+	 * 회원가입
+	 */
+	@PostMapping("/signup")
+	public ResponseEntity<Boolean> setSignUp(@CurrentUser
+	UserPrincipal userPrincipal, SignUpRequest signUpRequest, @RequestBody
+	MultipartFile imageFile) {
+
+		Boolean queryChk = userService.setSignUp(userPrincipal.getUser(), signUpRequest, imageFile);
+
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(queryChk);
+
+	}
 }
--- server/src/main/java/com/ttukttak/oauth/service/UserService.java
+++ server/src/main/java/com/ttukttak/oauth/service/UserService.java
@@ -1,42 +1,17 @@
 package com.ttukttak.oauth.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.ttukttak.common.exception.ResourceNotFoundException;
+import com.ttukttak.oauth.dto.SignUpRequest;
+import com.ttukttak.oauth.dto.UserDto;
 import com.ttukttak.oauth.entity.User;
-import com.ttukttak.oauth.entity.UserPrincipal;
-import com.ttukttak.oauth.repository.UserRepository;
 
-import lombok.RequiredArgsConstructor;
+public interface UserService {
 
-@Service
-@RequiredArgsConstructor
-public class UserService implements UserDetailsService {
+	UserDto getById(Long id);
 
-    private final UserRepository userRepository;
+	Boolean existsByName(String nickname);
 
-    @Override
-    @Transactional
-    public UserDetails loadUserByUsername(String email)
-            throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() ->
-                        new UsernameNotFoundException("User not found with email : " + email)
-        );
+	Boolean setSignUp(User user, SignUpRequest signUpRequest, MultipartFile imageFile);
 
-        return UserPrincipal.create(user);
-    }
-
-    @Transactional
-    public UserDetails loadUserById(Long id) {
-        User user = userRepository.findById(id).orElseThrow(
-            () -> new ResourceNotFoundException("User", "id", id)
-        );
-
-        return UserPrincipal.create(user);
-    }
 }
