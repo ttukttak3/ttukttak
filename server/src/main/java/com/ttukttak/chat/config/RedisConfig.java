c52743d32a24d43c14f9fa79ca952b4a565b5549
Songwonseok <dnjstjr0517@gmail.com>
2022-06-18 10:31:43 +0900

    Chore: redis 설정 변경

--- server/src/main/java/com/ttukttak/chat/config/RedisConfig.java
+++ server/src/main/java/com/ttukttak/chat/config/RedisConfig.java
@@ -1,8 +1,11 @@
 package com.ttukttak.chat.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -13,6 +16,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Configuration
 public class RedisConfig {
+	@Value("${spring.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.redis.port}")
+	private int redisPort;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(redisHost);
+		redisStandaloneConfiguration.setPort(redisPort);
+		return new LettuceConnectionFactory(redisStandaloneConfiguration);
+	}
 
 	/**
 	 * redis pub/sub 메시지를 처리하는 listener 설정
