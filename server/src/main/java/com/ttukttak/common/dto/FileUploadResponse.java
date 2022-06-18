bc29bbdb3e8c47d8cc5e08f49b548827b7b4caca
ANHYUCK8150 <blackhyuck@gmail.com>
2022-06-18 22:13:15 +0900

    chore: [BE] FileUploadResponse 어노테이션 추가

--- server/src/main/java/com/ttukttak/common/dto/FileUploadResponse.java
+++ server/src/main/java/com/ttukttak/common/dto/FileUploadResponse.java
@@ -1,17 +1,15 @@
 package com.ttukttak.common.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class FileUploadResponse {
 
 	String fileName;
 	String url;
 
-	public FileUploadResponse(String fileName, String url) {
-		this.fileName = fileName;
-		this.url = url;
-	}
 }
