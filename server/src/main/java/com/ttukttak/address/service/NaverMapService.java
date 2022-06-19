5cd36a8715281ffbe64fbb45a76cfdd846887302
ANHYUCK8150 <blackhyuck@gmail.com>
2022-06-19 20:41:23 +0900

    Chore: Service 명 변경 및 geocoding 추가

--- server/src/main/java/com/ttukttak/address/service/NaverMapService.java
+++ server/src/main/java/com/ttukttak/address/service/NaverMapService.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLEncoder;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -18,16 +19,18 @@ import com.ttukttak.address.dto.CoordinateRequest;
 import com.ttukttak.address.dto.TownDto;
 
 @Service
-public class ReverseGeocoding {
-	private String apiurl = "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc";
+public class NaverMapService {
+	private String ReverseGeocodingApi = "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc";
+	private String GeocodingApi = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode";
 	@Value("${naver.map.clientId}")
 	private String clientId;
 	@Value("${naver.map.clientSecret}")
 	private String clientKey;
 
-	public TownDto getLocation(CoordinateRequest CoordinateRequest) {
+	public TownDto getReverseGeocoding(CoordinateRequest CoordinateRequest) {
 
-		String api = apiurl + "?coords=" + CoordinateRequest.getLongitude() + "," + CoordinateRequest.getLatitude()
+		String api = ReverseGeocodingApi + "?coords=" + CoordinateRequest.getLongitude() + ","
+			+ CoordinateRequest.getLatitude()
 			+ "&orders=legalcode&output=json";
 		StringBuffer sb = new StringBuffer();
 		TownDto townDto = new TownDto();
@@ -86,5 +89,53 @@ public class ReverseGeocoding {
 		} catch (IOException e) {} catch (ParseException e) {}
 
 		return townDto;
+	}
+
+	public CoordinateRequest getGeocoding(String query) {
+		CoordinateRequest coordinateRequest = new CoordinateRequest();
+		try {
+			String addr = URLEncoder.encode(query, "utf-8");
+			String api = GeocodingApi + "?query=" + addr;
+			StringBuffer sb = new StringBuffer();
+			URL url = new URL(api);
+			HttpsURLConnection http = (HttpsURLConnection)url.openConnection();
+			http.setRequestProperty("Content-Type", "application/json");
+			http.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+			http.setRequestProperty("X-NCP-APIGW-API-KEY", clientKey);
+			http.setRequestMethod("GET");
+			http.connect();
+
+			InputStreamReader in = new InputStreamReader(http.getInputStream(), "utf-8");
+			BufferedReader br = new BufferedReader(in);
+
+			String line;
+			while ((line = br.readLine()) != null) {
+				sb.append(line).append("\n");
+			}
+
+			JSONParser parser = new JSONParser();
+			JSONObject jsonObject;
+			JSONObject jsonObject2;
+			JSONArray jsonArray;
+
+			jsonObject = (JSONObject)parser.parse(sb.toString());
+			jsonArray = (JSONArray)jsonObject.get("addresses");
+			for (int i = 0; i < jsonArray.size(); i++) {
+				jsonObject2 = (JSONObject)jsonArray.get(i);
+				if (null != jsonObject2.get("x")) {
+					coordinateRequest.setLongitude(Double.parseDouble(jsonObject2.get("x").toString()));
+				}
+				if (null != jsonObject2.get("y")) {
+					coordinateRequest.setLatitude(Double.parseDouble(jsonObject2.get("y").toString()));
+				}
+			}
+
+			br.close();
+			in.close();
+			http.disconnect();
+
+		} catch (IOException e) {} catch (ParseException e) {}
+
+		return coordinateRequest;
 	}
 }
