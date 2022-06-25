a97f9dcae9eede66454f4ea4fa7561b9afaffa51
ANHYUCK8150 <blackhyuck@gmail.com>
2022-06-26 01:44:31 +0900

    Feat: Controller 추가

---
+++ server/src/main/java/com/ttukttak/book/controller/BookController.java
@@ -0,0 +1,38 @@
+package com.ttukttak.book.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ttukttak.book.dto.BookInfoDto;
+import com.ttukttak.book.dto.InterParkRequest;
+import com.ttukttak.book.service.InterParkAPIService;
+import com.ttukttak.common.dto.PageResponse;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+
+@Api(value = "/api/book", description = "책 API")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/book")
+public class BookController {
+	private final InterParkAPIService interParkAPIService;
+
+	@ApiImplicitParam(name = "InterParkRequest", value = "인터파크 조회 값", required = true, dataType = "object", paramType = "body")
+	@ApiOperation(value = "인터파크 도서 조회")
+	@GetMapping("/interpark/search")
+	public ResponseEntity<PageResponse<BookInfoDto>> search(InterParkRequest interParkRequest) {
+
+		PageResponse<BookInfoDto> searchResult = interParkAPIService.search(interParkRequest);
+
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(searchResult);
+	}
+
+}
