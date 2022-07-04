a32b919b2af6ad226f733e1344bc7442d502e914
Songwonseok <dnjstjr0517@gmail.com>
2022-07-04 18:36:51 +0900

    feat: ChatGuest Entity 추가
    - 채팅방 중복 체크를 위해 책 대여자를 구분하기 위한 테이블

---
+++ server/src/main/java/com/ttukttak/chat/entity/ChatGuest.java
@@ -0,0 +1,39 @@
+package com.ttukttak.chat.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.ttukttak.book.entity.Book;
+import com.ttukttak.oauth.entity.User;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class ChatGuest {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "book_id")
+	private Book book;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Builder
+	public ChatGuest(Book book, User user) {
+		this.book = book;
+		this.user = user;
+	}
+}
