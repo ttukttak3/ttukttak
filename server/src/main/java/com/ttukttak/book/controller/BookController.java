456097b9e6b65058e2929d8e65d85e32d3aae284
ANHYUCK8150 <blackhyuck@gmail.com>
2022-07-08 16:52:29 +0900

    Fix: PUT -> PATCH 변경

--- server/src/main/java/com/ttukttak/book/controller/BookController.java
+++ server/src/main/java/com/ttukttak/book/controller/BookController.java
@@ -6,9 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -172,7 +172,7 @@ public class BookController {
 		@ApiImplicitParam(name = "status", value = "도서 상태값", required = true, dataType = "BookStatus", paramType = "body")
 	})
 	@ApiOperation(value = "도서 상태값 수정")
-	@PutMapping("/{bookId}/status")
+	@PatchMapping("/{bookId}/status")
 	public ResponseEntity<Boolean> updateBookStatus(
 		@PathVariable("bookId")
 		Long bookId,
@@ -192,7 +192,7 @@ public class BookController {
 		@ApiImplicitParam(name = "grade", value = "도서 등급", required = true, dataType = "BookGrade", paramType = "body")
 	})
 	@ApiOperation(value = "도서 등급 수정")
-	@PutMapping("/{bookId}/grade")
+	@PatchMapping("/{bookId}/grade")
 	public ResponseEntity<Boolean> updateBookGrade(
 		@PathVariable("bookId")
 		Long bookId, @RequestBody
