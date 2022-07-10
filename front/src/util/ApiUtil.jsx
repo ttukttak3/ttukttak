c08dcc0fb8f20425ea884fcce4caf9bbe1b48093
김민정 <minjeong930610@gmail.com>
2022-07-11 01:32:48 +0900

    주소 수정

--- front/src/util/ApiUtil.jsx
+++ front/src/util/ApiUtil.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-//const baseUrl = 'http://101.101.219.43:8080/';
-const baseUrl = 'http://27.96.134.65:8090/';
+const baseUrl = 'http://101.101.219.43:8080/';
 const accessToken = 'accessToken';
 const token = localStorage.getItem(accessToken);
 export const ACCESS_TOKEN = 'accessToken';
