00916d51ddc19dfdd0012a936ad11c84fb078afb
Songwonseok <dnjstjr0517@gmail.com>
2022-07-11 19:54:49 +0900

    feat: Rent 조회 관련 DTO 추가
    - RentCard: 대여/차입 내역 화면 데이터
    - RentedBook: 대여/차입 데이터 관련 Book 데이터

---
+++ server/src/main/java/com/ttukttak/rent/dto/RentedBook.java
@@ -0,0 +1,49 @@
+package com.ttukttak.rent.dto;
+
+import com.ttukttak.book.entity.Book;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class RentedBook {
+	private static final int RENT_PRICE = 2000;
+
+	private String subject;
+	private String author;
+	private String imageUrl;
+	private int deposit;
+	private Book.BookStatus status;
+
+	public int getRentPrice() {
+		return RENT_PRICE;
+	}
+
+	@Builder
+	private RentedBook(String subject, String author, String imageUrl, int deposit,
+		Book.BookStatus status) {
+		this.subject = subject;
+		this.author = author;
+		this.imageUrl = imageUrl;
+		this.deposit = deposit;
+		this.status = status;
+	}
+
+	public static RentedBook of(Book book) {
+		return RentedBook.builder()
+			.subject(book.getSubject())
+			.author(book.getAuthor())
+			.imageUrl(book.getThumbnail().getImageUrl())
+			.deposit(book.getDeposit())
+			.status(book.getStatus())
+			.build();
+	}
+}
