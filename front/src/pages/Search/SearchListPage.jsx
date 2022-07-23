da6ea5ee9bfa43f399a3c22ef87a1736abff4bca
KangSoYeon <aaa0311@naver.com>
2022-07-23 20:44:51 +0900

    feat: 도서 검색 header 제거, 검색 창 css 작업, 검색창 버튼 연결

--- front/src/pages/Search/SearchListPage.jsx
+++ front/src/pages/Search/SearchListPage.jsx
@@ -1,35 +1,53 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 import style from './SearchListPage.style';
 import bookApi from '../../util/BookApi';
 import Search from '../../assets/img/userInterFace/Search.png';
 import BookResultItem from './BookResultItem';
+import { setAllFalse } from '../../app/headerSlice';
+import keyboardArrowLeft from '../../assets/img/arrows/Keyboard_arrow_left.png';
+import clear from '../../assets/img/arrows/Clear.png';
 
 const SearchListPage = () => {
-  const { Wrapper, SearchBar, SearchBtn, BookList } = style;
+  const { Wrapper, SearchBarWrapper, SearchBar, SearchBtn, ClearBtn, BookList, SearchResult, NoResult, BackBtn } = style;
   const { interparkSearch } = bookApi;
   const [resultList, setResultList] = useState([]);
-  const [keyword, setKeyword] = useState();
+  const [keyword, setKeyword] = useState('');
   const [totalResultCount, setTotalResultCount] = useState(0);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    //header 끄기 옵션 추가
+    dispatch(setAllFalse());
   }, []);
 
   const searchKeyword = async () => {
     console.log(keyword);
     const result = await interparkSearch(1, keyword);
     setResultList([...resultList, ...result.contents]);
-    console.log(result.contents);
+    setTotalResultCount(result.pageSize * result.totalPages);
   };
 
   return (
     <Wrapper>
-      <SearchBar onChange={e => setKeyword(e.target.value)} value={keyword}></SearchBar>
-      <SearchBtn onClick={() => searchKeyword()}>
-        <img src={Search} alt="search" />
-      </SearchBtn>
-      <div>검색결과 ({totalResultCount})</div>
-      <BookList>{resultList.length <= 0 ? <div>찾고 싶은 책을 검색해보세요.</div> : resultList.map((item, index) => <BookResultItem item={item} key={index} />)}</BookList>
+      <SearchBarWrapper>
+        <BackBtn onClick={() => navigate(-1)}>
+          <img src={keyboardArrowLeft} alt={'이전 화면으로 이동'}></img>
+        </BackBtn>
+        <SearchBar onChange={e => setKeyword(e.target.value)} value={keyword}></SearchBar>
+        {keyword.length > 1 && (
+          <ClearBtn onClick={() => setKeyword('')}>
+            <img src={clear} alt="검색어 삭제" />
+          </ClearBtn>
+        )}
+        <SearchBtn onClick={() => searchKeyword()}>
+          <img src={Search} alt="search" />
+        </SearchBtn>
+      </SearchBarWrapper>
+      <SearchResult>검색결과 ({totalResultCount})</SearchResult>
+      <BookList>{resultList.length <= 0 ? <NoResult>찾고 싶은 책을 검색해보세요.</NoResult> : resultList.map((item, index) => <BookResultItem item={item} key={index} />)}</BookList>
     </Wrapper>
   );
 };
--- front/src/pages/Search/SearchListPage.style.jsx
+++ front/src/pages/Search/SearchListPage.style.jsx
@@ -5,21 +5,59 @@ const Wrapper = styled.div`
   flex-direction: column;
   font-size: 1.6rem;
   font-weight: 600;
+  margin: 1rem;
+`;
+
+const SearchBarWrapper = styled.div`
+  display: flex;
+  background: ${({ theme }) => theme.colors.$black1};
 `;
 
 const SearchBar = styled.input`
-  display: block;
-  width: 100%;
+  flex-grow: 1;
   height: 4rem;
   font-size: 1.6rem;
   font-weight: 400;
   color: ${({ theme }) => theme.colors.$white1};
-  background: ${({ theme }) => theme.colors.$black3Line};
+  background: ${({ theme }) => theme.colors.$black1};
 `;
 
 const SearchBtn = styled.a`
   height: 4rem;
   margin-top: 0.7rem;
+  margin-left: auto;
+  font-size: 1.2rem;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.$white3};
+  img {
+    display: block;
+    margin: 0 auto;
+    width: 2.4rem;
+    height: 2.4rem;
+    margin-bottom: 0.4rem;
+  }
+`;
+
+const BackBtn = styled.a`
+  height: 4rem;
+  margin-top: 0.7rem;
+  margin-right: auto;
+  font-size: 1.2rem;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.$white3};
+  img {
+    display: block;
+    margin: 0 auto;
+    width: 2.4rem;
+    height: 2.4rem;
+    margin-bottom: 0.4rem;
+  }
+`;
+
+const ClearBtn = styled.a`
+  height: 4rem;
+  margin-top: 0.7rem;
+  margin-right: auto;
   font-size: 1.2rem;
   font-weight: 400;
   color: ${({ theme }) => theme.colors.$white3};
@@ -36,11 +74,24 @@ const BookList = styled.li`
   display: flex;
   flex-wrap: wrap;
   flex-direction: row;
-  border-bottom: 0.3px solid ${({ theme }) => theme.colors.$whiteLine4};
   margin: 1rem;
   font-size: 2rem;
 `;
 
-const style = { Wrapper, SearchBar, SearchBtn, BookList };
+const SearchResult = styled.div`
+  margin-right: auto;
+  padding: 0.5rem;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.$white3};
+`;
+
+const NoResult = styled.div`
+  color: ${({ theme }) => theme.colors.$white3};
+  margin: auto;
+  font-size: 1.5rem;
+  margin-top: 15rem;
+`;
+
+const style = { Wrapper, SearchBarWrapper, SearchBar, SearchBtn, ClearBtn, SearchResult, BookList, NoResult, BackBtn };
 
 export default style;
